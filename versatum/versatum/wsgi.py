6ab4def1186bdce920eb860be90dfffffd5a0608
Eduardo Sant' Anna Martins <eduardomartins1993@gmail.com>
2017-09-16 23:19:22 -0300

    WSGI conf

--- versatum/versatum/wsgi.py
+++ versatum/versatum/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "versatum.settings")
+sys.path.insert(0, '/var/www/versatum.org/website/versatum/')
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "versatum.settings"
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "versatum.settings")
 
 application = get_wsgi_application()
